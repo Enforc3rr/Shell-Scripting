38582c7845c4c8769a472f723b2ab77beac99f33
Prateek Kumar <59215581+Enforc3rr@users.noreply.github.com>
2021-09-04 16:49:02 +0530

    Update PingSweep.sh

--- PingSweep.sh
+++ PingSweep.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-read -p "Enter Your Subnet : " SUBNET 
-
-for IP in $(seq 1 10);
-do 
-    ping "$SUBNET.$IP" -n 1 
+#!/bin/bash                                           
+read -p "Enter The subnet : " SUBNET
+for ip in $(seq 1 10);                                  
+do                                                              
+        IPSTATUS=$(ping -c 1 $SUBNET.$ip| grep transmitted, | awk '{print $6}')
+        if [ $IPSTATUS = "100%" ];
+        then                                                          
+            echo " $SUBNET.$ip is not available"
+        else
+                echo " $SUBNET.$ip is available"
+        fi
 done
